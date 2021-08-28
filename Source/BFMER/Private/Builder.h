29c3f73e5fcbf5047b2e42a13b23e60fecfbeef0
DUMONT Lucas <contact@lucasdumont.com>
2021-08-28 02:07:07 +0200

    Add c++ class

---
+++ Source/BFMER/Private/Builder.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Unit.h"
+#include "Builder.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABuilder : public AUnit
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/BFMER/Private/Unit.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Unit.h"
+
+
+// Sets default values
+AUnit::AUnit()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	m_UnitMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("mesh"));
+	RootComponent = m_UnitMesh;
+}
+
+// Called when the game starts or when spawned
+void AUnit::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AUnit::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
